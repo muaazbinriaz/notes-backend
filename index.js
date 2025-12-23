4ddcd6e1d74a58166472967f1f90971aa7a23e11
muaaz <muaazbinriaz2000@gmail.com>
2025-12-23 19:52:04 +0500

    serverless functions

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ let cors = require("cors");
 app.use(cors({ origin: "*" }));
 app.use("/api/website/notes", noteRouter);
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from Express on Vercel!' });
+});
 
 
 // connect to MongoDB
