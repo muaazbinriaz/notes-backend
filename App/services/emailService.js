ab06a2d63f77a58e7163fdc37768b0abfada5858
muaaz <muaazbinriaz2000@gmail.com>
2026-01-29 15:12:59 +0500

    updated

--- App/services/emailService.js
+++ App/services/emailService.js
@@ -10,19 +10,33 @@ const transporter = nodemailer.createTransport({
     pass: process.env.SMTP_PASS,
   },
 });
-const sendBoardInvite = async (to, boardTitle, inviterName, boardId) => {
+
+const sendBoardInvite = async (to, boardTitle, boardId) => {
   const inviteLink = `${process.env.FRONTEND_URL}/home/${boardId}`;
   await transporter.sendMail({
     from: `"MERN Notes App" <${process.env.SMTP_USER}>`,
     to,
-    subject: `${inviterName} invited you to "${boardTitle}"`,
-    text: `${inviterName} invited you to join the board "${boardTitle}".\n\n${inviteLink}`,
+    subject: `Invitation to board :  "${boardTitle}"`,
+    text: ` you are invited to join the board "${boardTitle}".\n\n${inviteLink}`,
     html: `
-      <p><strong>${inviterName}</strong> invited you to join the board <strong>${boardTitle}</strong>.</p>
+
       <a href="${inviteLink}">Accept Invite</a>
     `,
   });
 };
+// const sendBoardInvite = async (to, boardTitle, inviterName, boardId) => {
+//   const inviteLink = `${process.env.FRONTEND_URL}/home/${boardId}`;
+//   await transporter.sendMail({
+//     from: `"MERN Notes App" <${process.env.SMTP_USER}>`,
+//     to,
+//     subject: `${inviterName} invited you to "${boardTitle}"`,
+//     text: `${inviterName} invited you to join the board "${boardTitle}".\n\n${inviteLink}`,
+//     html: `
+//       <p><strong>${inviterName}</strong> invited you to join the board <strong>${boardTitle}</strong>.</p>
+//       <a href="${inviteLink}">Accept Invite</a>
+//     `,
+//   });
+// };
 
 module.exports = {
   sendBoardInvite,
